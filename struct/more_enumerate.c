24b46d2ff5208102b95191433504e97d2c231dc2
Truong Vinh <truongvinh@Truongs-MacBook-Pro.local>
2021-01-08 20:46:29 +0700

    Update cipher

--- struct/more_enumerate.c
+++ struct/more_enumerate.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-enum week{Mon=1, Tue=2, Wed=3, Thur=4, Fri=5, Sat=6, Sun=0};
+enum week{Mon=1, Tue=2, W=3, T=4, Fri=5, Sat=6, Sun=0};
  
 int main() {
     enum week day;
-    day = Wed;
-    printf("%d\n", day);
+    char a[10] = "WT";
+    for(int i=0; i<2; i++) {
+        day = a[i];
+        printf("%d\n", day);
+    }
     return 0;
 }
 
